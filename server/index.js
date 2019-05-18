316c90835fb32ef27c61245b58ba537739132035
Harsh <invokerharsh@gmail.com>
2019-05-18 20:14:40 +0530

    timer socket events on server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,26 @@
 const io = require('socket.io')();
 
+
+/*
+Socket works on events. We define actions based on the events trigger.
+Here, on connection establishment we pass client info in a callback.
+Which subscribes to an client-event named 'subscribeToTimer'. It also receives an interval
+value from client, which we then use to emit an event to the client in that interval.
+
+ client.on -> subscribe to an event
+ client.emit -> push an event
+
+*/
+
+io.on('connection', (client) => {
+    client.on("subscribeToTimer", (interval) => {
+        console.log(`client is subscribing to timer with interval ${interval}`);
+        setInterval(() => {
+           client.emit('timer', new Date()) 
+        }, interval);
+    })
+})
+
 const port = 8000;
 io.listen(8000);
 

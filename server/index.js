b74b2033ea0844ce9a2401a33ae73174cbcf8c91
Harsh <invokerharsh@gmail.com>
2019-05-19 02:39:42 +0530

    setup rethinkdb connection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,27 @@
 const io = require('socket.io')();
+const r = require('rethinkdb');
 
+//opens a new rethinkdb connection, returns a promise on success
+r.connect({
+    host: 'localhost',
+    port: 28015,
+    db: 'live-sketch',
+}).then((connection) => {
+    io.on('connection', (client) => {           // only accept new socket connections after rethinkdb connection
+        client.on("subscribeToTimer", (interval) => {
+            console.log(`client is subscribing to timer with interval ${interval}`);
+            r.tables('timers') //when client subscribes to this subscribeToTimer event, we open up a new query on rethinkdb
+            .changes()          // and lookout (subscribe) for changes
+            .run(connection)    // then run the query, passing it the connection from r.connect which returns a promise
+            .then((cursor) => {
+                cursor.each((err, timerRow) => {   // .each let's you know of every change and passes a callback
+                    if(err) return console.log(err)
+                    client.emit('timer', timerRow.new_val.timestamp);  // the row has the latest updated value on a property called new_val
+                });
+            });
+        });
+    });
+});
 
 /*
 Socket works on events. We define actions based on the events trigger.
@@ -10,16 +32,16 @@ value from client, which we then use to emit an event to the client in that inte
  .on -> subscribe to an event
  .emit -> publish an event
 
-*/
-
-io.on('connection', (client) => {
+ io.on('connection', (client) => {
     client.on("subscribeToTimer", (interval) => {
         console.log(`client is subscribing to timer with interval ${interval}`);
         setInterval(() => {
-           client.emit('timer', new Date()) 
+           client.emit('timer', new Date())
         }, interval);
     })
 })
+
+*/
 
 const port = 8000;
 io.listen(8000);

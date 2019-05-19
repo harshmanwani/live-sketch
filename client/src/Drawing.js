098f228c9d128778b5f3bdc97c77353395e5610b
Harsh <invokerharsh@gmail.com>
2019-05-19 17:01:55 +0530

    call publish lines with canvas ondraw

--- client/src/Drawing.js
+++ client/src/Drawing.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import Canvas from 'simple-react-canvas';
+import publishLine from './api';
 
-export const Drawing = ({ drawing }) => (
-    <div className="Drawing">
+export const Drawing = ({ drawing }) => {
+    
+    const handleDraw = (line) => {
+        publishLine({
+            drawingId: drawing.id,
+            line,
+        })
+    }
+    
+    
+    return <div className="Drawing">
         <div className="Drawing-title">{drawing.name}</div>
-        <Canvas drawingEnabled={true}/>
+        <Canvas 
+            drawingEnabled={true}
+            onDraw={handleDraw}
+        />
     </div>
-)
+}
